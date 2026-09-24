9fe8eae563d666556c09c532f375702e00b01406
elguapo87 <pglogovac@gmail.com>
2026-09-24 16:01:30 +0200

    Installed prisma adapter and created prisma client file

--- prisma/migrations/20260924131637_add_toll_worker_test_model/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "TollWorker" (
-    "id" SERIAL NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
-
-    CONSTRAINT "TollWorker_pkey" PRIMARY KEY ("id")
-);
